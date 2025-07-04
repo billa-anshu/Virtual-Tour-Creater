f6141e4522a91135cd0bfb43db5edec352c760a4
billa-anshu <billaanshu0@gmail.com>
2025-07-04 10:28:54 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,7 @@ app.config['TEMP_OUTPUT_FOLDER'] = TEMP_OUTPUT_FOLDER
 
 # --- Supabase Configuration ---
 SUPABASE_URL = 'https://fogqiruqayzamorywwkl.supabase.co'
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvZ3FpcnVxYXl6YW1vcnl3d2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NjAxODAsImV4cCI6MjA2NjUzNjE4MH0.fa0st9V3allcHbD-Nklnh9ajLHRXgwSXWMnxBhp81hA'
+SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvZ3FpcnVxYXl6YW1vcnl3d2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NjAxODAsImV4cCI6MjA2NjUzNjE4MH0.fa0st9V3allcHbD-Nklnh9ajYLRXgwSXWMnxBhp81hA'
 SUPABASE_BUCKET_NAME = "tour-images"
 SUPABASE_AUDIO_BUCKET_NAME = "tour-audio" # New: Supabase bucket for audio
 SUPABASE_PANORAMAS_TABLE = "panoramas"
